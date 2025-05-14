08d19260116e5c387bf8ca40567b0d7df225a4b3
saad zafar <codewithsaadzafar@gmail.com>
2025-05-14 17:35:04 +0500

    bug fix nav

--- Components/Nav.jsx
+++ Components/Nav.jsx
@@ -14,8 +14,8 @@ export default function Nav() {
     <div className = {isMobile ? style.navMobile : style.navPc}>
         <h1>Tech Logic.</h1>
         <ul>
-           {isMobile ? null :  <li onClick={()=>{route.push('/')}}>Home</li>}
-            {isMobile ? <li>Domains</li> : <li onClick={()=>{route.push('/domains')}}>Our Domains</li>}
+         <li onClick={()=>{route.push('/')}}>Home</li>
+            {isMobile ? <li onClick={()=>{route.push('/domains')}}>Domains</li> : <li onClick={()=>{route.push('/domains')}}>Our Domains</li>}
             <li onClick={()=>{route.push('/projects')}}>Projects</li>
             <li><a href="mailto:thetechlogicofficial@gmail.com">Mail</a></li>
            
